f35924fc8fab956d199c68d32ff791544d92d0e8
ParkSohyunee <sohyunee016@gmail.com>
2023-07-20 16:38:23 +0900

    Day24 문제풀이

---
+++ 프로그래머스/LV.1/9. 약수의 개수와 덧셈.js
@@ -0,0 +1,22 @@
+// 1. 문제
+// 두 정수 left와 right가 매개변수로 주어집니다.
+// left부터 right까지의 모든 수들 중에서, 약수의 개수가 짝수인 수는 더하고,
+// 약수의 개수가 홀수인 수는 뺀 수를 return 하도록 solution 함수를 완성해주세요.
+
+// 2. 풀이
+function solution(left, right) {
+  let count = 0;
+  let result = 0;
+
+  for (let i = left; i <= right; i++) {
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) count++;
+    }
+    count % 2 === 0 ? (result += i) : (result -= i);
+    count = 0;
+  }
+  return result;
+}
+
+solution(24, 27); // 52
+solution(13, 17); // 43
