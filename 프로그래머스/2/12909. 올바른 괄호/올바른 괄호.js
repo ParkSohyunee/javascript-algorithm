04f483f0c443609d6c5f5ca93a1dc9fbea864bd0
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-03-28 11:19:43 +0900

    [level 2] Title: 올바른 괄호, Time: 2.83 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,21 @@
+function solution(s) {
+  let sum = 1;
+
+  if (s[0] === ")") {
+    return false;
+  }
+
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] === "(") {
+      sum += 1;
+    } else {
+      sum -= 1;
+    }
+
+    if (sum < 0) {
+      return false;
+    }
+  }
+
+  return sum === 0 ? true : false;
+}
