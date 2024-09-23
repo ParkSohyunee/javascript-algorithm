6d3657896589f5817e87ce557b66a732a471a05b
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-09-23 14:33:05 +0900

    Time: 69 ms (54.09%), Space: 54.6 MB (72.6%) - LeetHub

--- leetcode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
+++ leetcode/Easy/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -2,8 +2,14 @@
  * @param {string} s
  * @return {string}
  */
+
+// 리팩토링
+// 1. 배열 대신 Set 자료구조 사용
+// 2. 모음임을 판별하는 조건을 변수로 분리
+// 3. start, end 둘다 모음일 때, 구조분해 할당으로 swap
+// 4. 불필요한 조건문 제거 및 수정
 function reverseVowels(s) {
-  const vowels = ["a", "e", "i", "o", "u"];
+  const vowels = new Set(["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]);
   const answer = s.split("");
 
   let start = 0;
@@ -13,21 +19,19 @@ function reverseVowels(s) {
     return s;
   }
 
-  while (end >= start) {
-    if (
-      vowels.includes(s[start].toLowerCase()) &&
-      vowels.includes(s[end].toLowerCase())
-    ) {
-      answer[start] = s[end];
-      answer[end] = s[start];
+  while (start < end) {
+    const isVowelStart = vowels.has(answer[start]);
+    const isVowelEnd = vowels.has(answer[end]);
+
+    if (isVowelStart && isVowelEnd) {
+      [answer[start], answer[end]] = [answer[end], answer[start]];
       start++;
       end--;
-    } else if (vowels.includes(s[start].toLowerCase())) {
-      end--;
-    } else if (vowels.includes(s[end].toLowerCase())) {
+    }
+    if (!isVowelStart) {
       start++;
-    } else {
-      start++;
+    }
+    if (!isVowelEnd) {
       end--;
     }
   }
