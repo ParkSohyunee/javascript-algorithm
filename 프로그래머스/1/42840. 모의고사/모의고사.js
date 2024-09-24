009ecba7aa75ca6abf6e2e9607734d9e93cf5116
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-09-24 15:00:15 +0900

    [level 1] Title: 모의고사, Time: 39.18 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -31,33 +31,3 @@ function solution(answers) {
   });
   return result.length === 1 ? result : result.sort((a, b) => a - b);
 }
-
-// 이전 풀이
-// function solution(answers) {
-//   const result = [];
-
-//   const math = {
-//     0: [1, 2, 3, 4, 5],
-//     1: [2, 1, 2, 3, 2, 4, 2, 5],
-//     2: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
-//   };
-
-//   for (let i = 0; i < Object.keys(math).length; i++) {
-//     let count = 0;
-//     for (let j = 0; j < answers.length; j++) {
-//       if (answers[j] === math[i][j % math[i].length]) {
-//         count += 1;
-//       }
-//     }
-//     result.push(count);
-//   }
-
-//   const findIndex = [];
-//   result.filter((el, index) => {
-//     if (el === Math.max(...result)) {
-//       return findIndex.push(index + 1);
-//     }
-//   });
-
-//   return findIndex.sort((a, b) => a - b);
-// }
