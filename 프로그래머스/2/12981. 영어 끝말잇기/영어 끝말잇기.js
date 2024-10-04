eff335db17a0d82edce5e35ec5e1e3ea15a560b0
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-10-04 14:31:19 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,26 +1,26 @@
+// 리팩토링
+// 1. 집합을 이용하기 위해 Set 객체 사용
+// 2. 끝말잇기 규칙을 검사하기 위한 변수 최소화
+
 function solution(n, words) {
-  let postLastWord;
-  let currentFirstWord;
+  const checkedWord = new Set();
+  let prevWord = words[0][0];
 
   for (let i = 0; i < words.length; i++) {
     let number = (i % n) + 1;
     let order = Math.ceil((i + 1) / n);
 
-    let slicedArray = words.slice(0, i);
     let isPassWordLength = words[i].length > 1;
 
-    if (!isPassWordLength || slicedArray.includes(words[i])) {
+    if (
+      !isPassWordLength ||
+      checkedWord.has(words[i]) ||
+      words[i][0] !== prevWord
+    ) {
       return [number, order];
     }
-
-    if (i === 0) continue;
-
-    postLastWord = words[i - 1][words[i - 1].length - 1];
-    currentFirstWord = words[i][0];
-
-    if (!isPassWordLength || postLastWord !== currentFirstWord) {
-      return [number, order];
-    }
+    prevWord = words[i].slice(-1);
+    checkedWord.add(words[i]);
   }
   return [0, 0];
 }
